360ea99112e79f2f5e42b9ad66d1c2b1d25aba68
metallyzed <pasjm94@gmail.com>
2024-08-26 21:23:35 -0500

    Adding index, css and assets

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Hash keys to be used
+// Keys to be used in encode
 const hashKeys = {
     'a' : 'ai',
     'e': 'enter',
@@ -18,22 +18,60 @@ const reverseKeys = {
 
 // Encode function
 function encode(text) {
-    let modifiedText = '';
-    for (let char of text) {
-        modifiedText += hashKeys[char] || char;
+    if (text === '') {
+        showPlaceholder();
+    } else {
+        let modifiedText = '';
+        for (let char of text) {
+            modifiedText += hashKeys[char] || char;
+        }
+        showOutput(modifiedText);
+        return modifiedText;
     }
-    return modifiedText
 }
 
 // Decode function
 function decode(text) {
-    let modifiedText = text;
-    for (let reverseKey in reverseKeys) {
-        modifiedText = modifiedText.split(reverseKey).join(reverseKeys[reverseKey])
+    if (text === '') {
+        showPlaceholder();
+    } else {
+        let modifiedText = text;
+        for (let reverseKey in reverseKeys) {
+            modifiedText = modifiedText.split(reverseKey).join(reverseKeys[reverseKey])
+        }
+        showOutput(modifiedText);
+        return modifiedText;
     }
-    return modifiedText;
 }
 
+// Show output
+function showOutput(text) {
+    document.getElementById('outputPlaceholder').style.display = 'none';
+    let outputTextArea = document.getElementById('outputText');
+    outputTextArea.style.display = 'block';
+    outputTextArea.value = text;
+    document.getElementById('copy-button').style.display = 'block';
+}
+
+// Show placeholder
+function showPlaceholder() {
+    document.getElementById('outputText').style.display = 'none';
+    document.getElementById('copy-button').style.display = 'none';
+    document.getElementById('outputPlaceholder').style.display = 'flex';
+}
+
+// Copy output text
+function copyOutputText() {
+    var outputText = document.getElementById("outputText");
+
+    navigator.clipboard.writeText(outputText.value);
+}
+
+// Ensure these functions are available globally
+window.encode = encode;
+window.decode = decode;
+
+/*
 // Encode example
 const original = "gato";
 const encodeText = encode(original);
@@ -43,3 +81,4 @@ console.log(encodeText);
 const originalEncoded = "gaitober";
 const decodedText = decode(originalEncoded);
 console.log(decodedText);
+*/
